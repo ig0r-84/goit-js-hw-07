2d1e6a55a0fe5c9e6b3ccd8e61b8537e2bf3ce49
ig0r-84 <coshilnick@gmail.com>
2026-06-23 00:35:15 +0300

    add 1 tesk

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${categoriesList.length}`);
 categoriesList.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
-  const elementsCount = category.querySelectorAll("ul li").length;
+  const elementCount = category.querySelectorAll("ul li").length;
   console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
+  console.log(`Elements: ${elementCount}`);
 });
