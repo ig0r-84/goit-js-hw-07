ad3505f4d2e96256d5b31dcdce3a86e608b0f8c9
ig0r-84 <coshilnick@gmail.com>
2026-07-03 15:14:44 +0300

    add 1 task

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,7 @@ console.log(`Number of categories: ${categoriesList.length}`);
 categoriesList.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
   const elementCount = category.querySelectorAll("ul li").length;
+  console.log(category.querySelectorAll("ul li"));
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${elementCount}`);
 });
