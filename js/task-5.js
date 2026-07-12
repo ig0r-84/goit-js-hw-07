3a0a2c3e0e23028a67dab66b5f0965bc577f3294
ig0r-84 <coshilnick@gmail.com>
2026-07-12 13:16:45 +0300

    add 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const button = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
